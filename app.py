7fe74e7d8dfcf6ee1cfc250e83cbd0d5df5167c5
lawalsegun2025 <lawalsegun2025@gmail.com>
2023-05-29 14:13:30 +0100

    in progress

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 import streamlit as st
-import preprocessing
+import preprocess
 import re 
 import stats 
 import matplotlib.pyplot as plt 
 import numpy as np 
+
+st.sidebar.title("Whatsapp Chat Analyzer")
+
+# upload a file
+uploaded_file = st.sidebar.file_uploader("choose a file")
+
+if uploaded_file is not None:
+    bytes_data = uploaded_file.getvalue()
+
+    # converting the bytecode to the text-file
+    data = bytes_data.decode("utf-8")
+
+    # sending file data to the preprocessing function for further functioning
+    df = preprocess.preprocess(data)
