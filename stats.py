9c152e8127210ce2d159b51ed0bb81a34b748b53
lawalsegun2025 <lawalsegun2025@gmail.com>
2023-06-01 14:50:38 +0100

    completed fetch_stats function

---
+++ stats.py
@@ -0,0 +1,32 @@
+from urlextract import URLExtract
+import pandas as pd 
+from collection import Counter 
+from wordcloud import WordCloud
+import emoji
+
+extract = URLExtract()
+
+def fetch_stats(selected_user, df):
+
+    # if the selected user is a specific user,then make changes in the 
+    # dataframe, else do not make any changes
+    if selected_user != "Overall":
+        df = df[df["User"] == selected_user]
+
+    # Get the number of messages
+    num_messages = df.shape[0]
+
+    # Get the number of words
+    words = []
+    for message in df["Message"]:
+        words.extend(message.split())
+
+    # Get the number of media files shared
+    media_omitted = df[df["Message"] == "<Media omitted>"]
+
+    # Get number of links shared
+    links = []
+    for message in df["Message"]:
+        links.extend(extract.find_urls(message))
+
+    return num_messages, len(words), media_omitted.shape[0], len(links)
