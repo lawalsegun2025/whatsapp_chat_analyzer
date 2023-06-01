d0a9feddda796a6d6f03410cf65b8d2f3e2340b1
lawalsegun2025 <lawalsegun2025@gmail.com>
2023-06-01 10:07:48 +0100

    added functions

--- preprocess.py
+++ preprocess.py
@@ -2,4 +2,17 @@ import streamlit as st
 import numpy as np 
 import seaborn as sns 
 import pandas as pd 
-import re
+import re 
+
+# This function separetes the time from the date
+def get_date_and_time(string):
+    string = string.split(",")
+    date, time = string[0], string[1]
+    time = time.split("-")
+    time = time[0].strip()
+    
+    return date+" "+time 
+
+def get_string(text):
+    return text.split('\n')[0]
+
