9be264b0719f0cd40df6fbe66fbccb6e5fb941e6
lawalsegun2025 <lawalsegun2025@gmail.com>
2023-06-01 13:56:31 +0100

    making progress

--- app.py
+++ app.py
@@ -32,4 +32,12 @@ if uploaded_file is not None:
     user_list.sort()
 
     # include overall. This is responsible for showcasing the overall chat analysis
-    user_list.insert(0, "overall")
+    user_list.insert(0, "Overall")
+
+    # select and display analysis of user
+    selected_user = st.sidebar.selectbox(
+        "Show analysis with respect to", user_list
+    )
+
+    st.title("WhatsApp Chat Analysis for " + selected_user)
+    if st.sidebar.button("Show Analysis"):
