698f494284bc5edfe87a9655c3e4630722179499
lawalsegun2025 <lawalsegun2025@gmail.com>
2023-06-02 16:12:24 +0100

    common words

--- stats.py
+++ stats.py
@@ -3,6 +3,14 @@ import pandas as pd
 from collection import Counter 
 from wordcloud import WordCloud
 import emoji
+import re
+import string 
+import nltk
+pd.set_option('display.max_colwidth', 100)
+
+stopwords = nltk.corpus.stopwords.words("english")
+wn = nltk.WordNetLemmatizer()
+punct = string.punctuation
 
 extract = URLExtract()
 
@@ -61,3 +69,48 @@ def create_word_cloud(selected_user, wc_df):
     return wc
 
 # get most common words as a dataframe
+
+# Clean text
+def clean_text(text):
+    # Remove punctuations 
+    text = "".join([word.lower() for word in text if word not in punct])
+    
+    # Remove any other signs
+    text = " ".join(re.split('\W+', text))
+    
+    # Remove numerial leaving only aplabets
+    text = re.sub('[^A-Za-z]+', ' ', str(text))
+    
+    # Remove links and websites
+    text = re.sub(r"http\S+", "", text)
+    text = re.sub(r"www.\S+", "", text)
+    
+    text = text.split()
+    
+    # Remove stopwords
+    text = [word for word in text if word not in stopwords]
+    
+    # lemmatize
+    text = [wn.lemmatize(word) for word in text]
+    
+    #
+    text = " ".join([word for word in text])
+    
+    return text
+
+def get_common_words(selected_user, wc_df):
+
+    # clean message
+    wc_df["clean_message"] = wc_df["Message"].apply(clean_text)
+
+    # Get top 20 words
+    if selected_user != "Overall":
+        wc_df = wc_df[wc_df["User"] == selected_user]
+
+    words = []
+
+    for message in wc_df["clean_message"]:
+            words.extend(message.split())
+
+    most_common_df = pd.DataFrame(Counter(words).most_common(20)).sort_values(1)
+    return most_common_df
